20e37d50d27a4b37602fce09230ed0d165156e27
Edon Sekiraqa <edon.sekiraqa@faculty.cacctus.education>
2020-12-11 20:36:23 +0100

    Manu functionalities in JS

--- jquery-example/index.js
+++ jquery-example/index.js
@@ -1,5 +1,6 @@
 // global variables
 var menuItems = [];
+var deletedMenuItems = [];
 
 // events
 
@@ -8,6 +9,22 @@ $("#add_new_button").click(function(){
 });
 $("#new_menu_item_close_button").click(function () {
     $("#new_menu_item_form").css("display", "none");
+});
+$("#search_box").on('keyup', function(){
+    var searchText = $(this).val();
+
+    if(searchText.trim().length == 0){
+        populateTable("#menu_items_table", menuItems, false);
+        return;
+    }
+
+    var foundItems = [];
+    for(var i=0; i<menuItems.length; i++){
+        if(menuItems[i].productName.includes(searchText)){
+            foundItems.push(menuItems[i]);
+        }
+    }
+    populateTable("#menu_items_table", foundItems, false);
 });
 $("#new_menu_item_add_button").click(function () {
     var name = $("#new_menu_item_form_name").val();
@@ -31,11 +48,11 @@ $("#new_menu_item_add_button").click(function () {
     $("#new_menu_item_form_name").val("");
     $("#new_menu_item_form_price").val("");
 
-    populateTableFromMenuItems();
+    populateTable("#menu_items_table", menuItems, false);
 });
 
-function populateTableFromMenuItems(){
-    $("#menu_items_table").html("");
+function populateTable(tableId, items, areDeleted){
+    $(tableId).html("");
 
     var template =
     "<tr>"+
@@ -44,24 +61,47 @@ function populateTableFromMenuItems(){
     "        <span>{product_price}</span>"+
     "    </td>"+
     "    <td>"+
-    "        <button onclick='deleteItem({product_idx})' style='color: red'>DELETE</button>"+
+        (
+            areDeleted ?
+             "<button onclick='revertItem({product_idx})' style='color: orange'>REVERT</button>" :
+    "        <button onclick='deleteItem({product_idx})' style='color: red'>DELETE</button>"
+        )+
     "    </td>"+
     "</tr>";
 
-    if(menuItems.length == 0){
+    if (items.length == 0){
         return;
     }
 
-    for(var i=0; i<menuItems.length; i++){
+    items = items.sort(function(a, b){
+        if(a.productPrice == b.productPrice){
+            return 0;
+        }else if (a.productPrice < b.productPrice){
+            return -1;
+        }
+
+        return 1;
+    });
+
+    for (var i = 0; i < items.length; i++){
         var row = template;
-        row = row.replace("{product_name}", menuItems[i].productName);
-        row = row.replace("{product_price}", menuItems[i].productPrice);
+        row = row.replace("{product_name}", items[i].productName);
+        row = row.replace("{product_price}", items[i].productPrice);
         row = row.replace("{product_idx}", i);
-        $("#menu_items_table").append(row);
+        $(tableId).append(row);
     }
 }
 
-function deleteItem(idx){
+function deleteItem(idx) {
+    deletedMenuItems.push(menuItems[idx]);
     menuItems.splice(idx, 1);
-    populateTableFromMenuItems();
+    populateTable("#menu_items_table", menuItems, false);
+    populateTable("#deleted_menu_items_table", deletedMenuItems, true);
+}
+
+function revertItem(idx){
+    menuItems.push(deletedMenuItems[idx]);
+    deletedMenuItems.splice(idx, 1);
+    populateTable("#menu_items_table", menuItems, false);
+    populateTable("#deleted_menu_items_table", deletedMenuItems, true);
 }
